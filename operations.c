2372652d4e2726900bc53c4a5ebcae90edc71eac
EgribasSaba <segribas@student.42heilbronn.de>
2026-03-06 23:34:07 +0100

    implement first swap for sa and sb and both ss.

--- operations.c
+++ operations.c
@@ -6,7 +6,36 @@
 /*   By: segribas <segribas@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 22:51:26 by segribas          #+#    #+#             */
-/*   Updated: 2026/03/06 22:51:28 by segribas         ###   ########.fr       */
+/*   Updated: 2026/03/06 23:33:20 by segribas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "push_swap.h"
+
+void	sa(t_stack *a)
+{
+	int tmp;
+	tmp = a->top->value;
+	a->top->value = a->top->next->value;
+	a->top->next->value = tmp;
+	write(1, "sa\n", 3);
+}
+void	sb(t_stack *b)
+{
+	int tmp;
+	tmp = b->top->value;
+	b->top->value = b->top->next->value;
+	b->top->next->value = tmp;
+	write(1,"sb\n", 3);
+}
+void	ss(t_stack *a, t_stack *b)
+{
+	int tmp;
+	tmp = a->top->value;
+	a->top->value = a->top->next->value;
+	a->top->next->value = tmp;
+	tmp = b->top->value;
+	b->top->value = b->top->next->value;
+	b->top->next->value = tmp;
+	write(1, "ss\n", 3);
+}
